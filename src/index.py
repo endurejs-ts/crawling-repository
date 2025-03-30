10b51af13c57f65eaaf38d0af3ffd7dc55f4ed0e
resforxofficial <paedeogim@gmail.com>
2025-03-31 03:09:25 +0900

    successfully remodel

---
+++ src/index.py
@@ -0,0 +1,51 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import urllib.parse
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-gpu")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+main = WebDriverWait(driver, 10)
+
+# //*[@id="anchorBoxId_1665"]/a //*[@id="anchorBoxId_2032"]/a
+# itemList = driver.find_element(By.XPATH, '//*[@id="contents"]/div[3]/div/ul/li/div')
+# allUL = itemList.find_elements(By.TAG_NAME, 'ul')
+# # //*[@id="anchorBoxId_1665"]/a #anchorBoxId_1665 > a
+# # #anchorBoxId_2032 > a
+# all_A = allUL[1].find_elements(By.XPATH, '//*[starts-with(@id, "anchorBoxId")]/a')
+# result_a1 = []
+# for aa in all_A:
+#     result_a1.append(urllib.parse.unquote(aa.get_attribute("href")))
+
+# with open("../dist/admin/head/href.json", "w", encoding="utf-8") as f:
+#     json.dump(result_a1, f, ensure_ascii=False)
+
+# driver.get("https://www.kakamuka.com/product/list.html?cate_no=122")
+# content_box = driver.find_element(By.XPATH, '//*[@id="contents"]/div[5]')
+
+def getLink(end_num: int, cate_num: int, fp: str):
+    for i in range(1, end_num):
+        result_a2 = []
+        driver.get(f"https://www.kakamuka.com/product/list.html?cate_no={cate_num}&page={i}")
+        main.until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]/div[5]')))
+        content_box = driver.find_element(By.XPATH, '//*[@id="contents"]/div[5]')
+        ul = content_box.find_element(By.XPATH, '//*[@id="contents"]/div[5]/div[2]/ul')
+
+        all_a = ul.find_elements(By.XPATH, '//*[starts-with(@id, "anchorBoxId")]/div[1]/a')
+        for aa in all_a:
+            result_a2.append(urllib.parse.unquote(aa.get_attribute("href")))
+
+        result_a2 = result_a2[7:]
+
+        with open(f"{fp}/bottom/href/href{i}.json", "w", encoding="utf-8") as f:
+            json.dump(result_a2, f, ensure_ascii=False)
+
+getLink(17, 88, "../dist/noadmin/88")
+driver.quit()
---
+++ src/main.py
@@ -0,0 +1,88 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep
+# from dotenv import load_dotenv
+# import os
+from selenium.common.exceptions import NoSuchElementException
+
+# load_dotenv()
+
+# ID = os.getenv("ID")
+# PWD = os.getenv("PWD")
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-gpu")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+# driver.get("https://www.kakamuka.com")
+# sleep(1)
+
+main = WebDriverWait(driver, 10)
+# main.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="JS_topMenu"]/li[1]/a[1]')))
+# login_button = driver.find_element(By.XPATH, '//*[@id="JS_topMenu"]/li[1]/a[1]')
+# login_button.click()
+# sleep(2)
+
+# main.until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]')))
+
+# input1 = driver.find_element(By.XPATH, '//*[@id="member_id"]')
+# input1.send_keys(ID)
+# sleep(1.3)
+
+# input2 = driver.find_element(By.XPATH, '//*[@id="member_passwd"]')
+# input2.send_keys(PWD)
+# sleep(2)
+
+# login_button_form = driver.find_element(By.XPATH, '//form[starts-with(@id, "member_form_")]/div/div/fieldset/a')
+# login_button_form.click()
+
+# sleep(3)
+
+def getOpLink(end_num, fp):
+    for v in range(1, end_num):
+        with open(f"{fp}/href/href{v}.json", "r", encoding="utf-8") as f:
+            content = json.load(f)
+
+        head_result = []
+        for ctt in content:
+            driver.get(ctt)
+            main.until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]')))
+            sleep(0.5)
+
+            img_link = driver.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[1]/div[1]/div[1]/a/img').get_attribute("src")
+            title = driver.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[3]/div[2]/h2').text
+            price = driver.find_element(By.XPATH, '//*[@id="span_product_price_text"]').text.replace("원", "")
+            category_code = driver.find_element(By.XPATH, '//*[@id="contents"]/div[1]/ol').text
+            ctg_result = " > ".join(category_code.split("\n"))
+
+            try:
+                img_link_2 = driver.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[1]/div[2]/ul/li[2]/img').get_attribute("src")
+            except NoSuchElementException:
+                img_link_2 = ""
+
+            try:
+                img_link_3 = driver.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[1]/div[2]/ul/li[3]/img').get_attribute("src")
+            except NoSuchElementException:
+                img_link_3 = ""
+
+            head_result.append({
+                "modelName": ctt,
+                "name": title,
+                "categoryCode": ctg_result,
+                "price": price,
+                "img1Url": img_link,
+                "img2Url": img_link_2,
+                "img3Url": img_link_3
+            })
+
+        with open(f"{fp}/data/data{v}.json", "w", encoding="utf-8") as f2:
+            json.dump(head_result, f2, ensure_ascii=False, indent=4)
+
+getOpLink(17, "../dist/noadmin/88/bottom")
+driver.quit()
